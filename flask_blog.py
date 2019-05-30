5a1dafc120136806bc032719cfc4b1e640484760
ameidatou <imed.mnif@gmail.com>
2019-05-30 01:33:26 +0100

    Add simple templates and rendering
    * template inheritance
    * bootstrap css

--- flask_blog.py
+++ flask_blog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Imed Mnif',
+        'title': 'First Blog Post',
+        'content': 'First post content',
+        'date_posted': 'May 31, 2019'
+    },
+    {
+        'author': 'Omar Mnif',
+        'title': 'Second Blog Post',
+        'content': 'Second post content',
+        'date_posted': 'May 30, 2019'
+    }
+]
+
 
 @app.route("/home")
 @app.route("/")
 def home():
-        return "<h1>Home Page</h1>"
+        return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-        return "<h1>About Page</h1>"
+        return render_template("about.html", title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
